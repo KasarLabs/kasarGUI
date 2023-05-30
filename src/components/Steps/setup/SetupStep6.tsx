4bbc33c86a72b6ce760a7859a9b9f0d1782a9582
jb1011 <demenet.jb@hotmail.fr>
2023-05-30 18:19:46 +0200

    call function fetch every 5 seconds and stops when data is fetched

--- src/components/Steps/setup/SetupStep6.tsx
+++ src/components/Steps/setup/SetupStep6.tsx
@@ -5,19 +5,12 @@ import { Button, ButtonSmall, OutlineButton } from '../../s-components/Buttons'
 import { Card } from '../../s-components/Card'
 import { H1, Text, TextGray } from '../../s-components/Texts'
 import styled from 'styled-components'
-import ArrowGray from '../../../assets/icons/ArrowGray.png'
-import SetupGif from '../../../assets/gif/setup.png'
-import { Input } from '@/components/s-components/Input'
-import { SpaceBetween } from '@/components/s-components/Flex'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import confetti from 'canvas-confetti'
-import { Separator } from '@/components/s-components/utils'
+import { Separator, SeparatorSM } from '@/components/s-components/utils'
 import axios from 'axios'
 import { SERVER_NODE_API } from '@/constants'
-
-const Image = styled.img`
-  max-width: 180px;
-`
+import { TailSpin } from 'react-loader-spinner'
 
 const Rows = styled.div`
   display: flex;
@@ -38,10 +31,16 @@ const Row = styled.div`
   }
 `
 
-const Inputs = styled.div`
+const DisplayNode = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 15px;
+  gap: 10px;
+  box-shadow: 0px 2px 6px 2px rgba(0, 0, 0, 0.25);
+  padding: 10px;
+  border-radius: 5px;
+p{
+margin: 0;
+}
 `
 
 const Block = styled.div`
@@ -56,14 +55,26 @@ type PreviousStepProps = {
 }
 
 function Step6({ nextStep, previousStep, uuid }: PreviousStepProps) {
+  const [loading, setLoading] = useState(false)
+  const [nodes, setNodes] = useState([])
+
   useEffect(() => {
+    let intervalId: NodeJS.Timeout;
     const callNode = async () => {
-      const { data } = await axios.post(`${SERVER_NODE_API}/node/getAllOf?provider_id=${uuid}`, {
-      });
-      console.log(data)
-    }
-    callNode()
-  }, [])
+      const { data } = await axios.get(`${SERVER_NODE_API}/node/getAllOf?provider_id=${uuid}`);
+      console.log(data);
+      if (data.length) {
+        clearInterval(intervalId); // Stop calling the API once data is not empty
+        setLoading(false);
+        setNodes(data);
+      }
+    };
+    setLoading(true);
+    callNode();
+    intervalId = setInterval(callNode, 5000); // Call the API every 5 seconds
+    return () => clearInterval(intervalId); // Clean up the interval on unmount
+  }, [uuid]);
+
   return (
     <Card>
       <Rows>
@@ -72,6 +83,35 @@ function Step6({ nextStep, previousStep, uuid }: PreviousStepProps) {
           <Text>Please wait, synchronization is in progress...</Text>
         </Row>
         <Separator />
+        {loading &&
+          <TailSpin
+            height="100"
+            width="100"
+            color="#000"
+            ariaLabel="tail-spin-loading"
+            radius="2"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        }
+        {!loading &&
+          <>
+            <Text>My nodes:</Text>
+            <SeparatorSM />
+            <DisplayNode>
+              {nodes && nodes.map((node, index) => {
+                return (
+                  <>
+                    <p>Node {index + 1}</p>
+                    <p>RPC: {node.RPC}</p>
+                    <p>Block: {node.L2.Block}</p>
+                  </>
+                )
+              })}
+            </DisplayNode>
+          </>
+        }
         {/* <Inputs>
           <ButtonSmall onClick={() => nextStep(-1)}>Home</ButtonSmall>
         </Inputs> */}
