fa3889c8b55c12483440669420301f9543fdc8b8
jb1011 <demenet.jb@hotmail.fr>
2023-06-21 09:39:06 +0200

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,6 @@ import Setup3 from './components/Steps/setup/SetupStep3'
 import Setup4 from './components/Steps/setup/SetupStep4'
 import Setup5 from './components/Steps/setup/SetupStep5'
 import Setup6 from './components/Steps/setup/SetupStep6'
-import Setup7 from './components/Steps/setup/SetupStep7'
-import Setup8 from './components/Steps/setup/SetupStep8'
-import UpdateStep1 from './components/Steps/update/UpdateStep1'
-import UpdateStep2 from './components/Steps/update/UpdateStep2'
-import UpdateStep3 from './components/Steps/update/UpdateStep3'
-import UpdateStep4 from './components/Steps/update/UpdateStep4'
 import { useState } from 'react'
 import LoginStep from './components/Steps/setup/LoginStep'
 import Verification from './components/Steps/setup/Verification'
@@ -69,12 +63,6 @@ function App() {
       {step === 4 && <Setup4 nextStep={nextStep} previousStep={previousStep} jsonData={jsonData} pathSD={pathSD} setPathSD={setPathSD} />}
       {step === 5 && <Setup5 nextStep={nextStep} previousStep={previousStep} jsonData={jsonData} />}
       {step === 6 && <Setup6 nextStep={nextStep} previousStep={previousStep} uuid={uuid} jsonData={jsonData} />}
-      {step === 7 && <Setup7 nextStep={nextStep} previousStep={previousStep} />}
-      {step === 15 && <Setup8 nextStep={nextStep} previousStep={previousStep} />}
-      {step === 11 && <UpdateStep1 nextStep={nextStep} previousStep={previousStep} />}
-      {step === 12 && <UpdateStep2 nextStep={nextStep} previousStep={previousStep} />}
-      {step === 13 && <UpdateStep3 nextStep={nextStep} previousStep={previousStep} />}
-      {step === 14 && <UpdateStep4 nextStep={nextStep} previousStep={previousStep} />}
       {step === 9 && <Verification nextStep={nextStep} />}
       {step === 10 && <ResetPassword nextStep={nextStep} previousStep={previousStep} setEmail={setEmail} email={email} />}
     </Main>
